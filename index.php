fbc231449163ddf58356291e7c0d159cb1dc6430
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-10 15:07:58 -0300

    Finalizado o site espero que gostem

--- index.php
+++ index.php
@@ -442,7 +442,6 @@
   </div>
   <!-- Fim do Jombontron -->
 
-
   <!--Inicio Formulário -->
   <div class="container">
     <div class="row">
@@ -454,18 +453,182 @@
 
     <div class="row justify-content-center mb-5">
 
-      <div class="col-sm-12 col-md-10 col-lg-8"></div>
-      <form>
-        <div class="form-row">
-          <div class="form-group col-sm-6">
-            <label for="input-nome"></label>
+      <div class="col-sm-12 col-md-10 col-lg-8">
+        <form>
+          <div class="form-row">
+            <div class="form-group col-sm-6">
+              <label for="inputNome">Nome</label>
+              <input type="text" class="form-control" id="inputNome" placeholder="Digite seu Nome" required>
+            </div>
+
+            <div class="form-group col-sm-6">
+              <label for="inputSobrenome">Sobrenome</label>
+              <input type="text" class="form-control" id="inputNome" placeholder="Digite seu Sobrenome" required>
+            </div>
           </div>
 
-        </div>
-      </form>
+          <div class="form-row">
+            <div class="form-group col-sm-3">
+              <label for="inputRg">RG</label>
+              <input type="number" class="form-control" id="inputRg" placeholder="Digite seu RG" maxlength="13" required>
+
+            </div>
+            <div class="form-group col-sm-3">
+              <label for="inputCPF">CPF</label>
+              <input type="number" class="form-control" id="inputCPF" placeholder="Digite seu CPF" maxlength="13" required>
+
+            </div>
+            <div class="form-group col-sm-3">
+              <label for="inputDate">Data de Nacimento</label>
+              <input type="date" class="form-control" id="inputDate" placeholder="" required>
+            </div>
+
+          </div>
+
+          <div class="form-row">
+
+            <div class="form-group col-sm-6">
+              <label for="inputEnd">Endereço</label>
+              <input type="text" class="form-control" id="inputEnd" placeholder="Digite seu Enderço" required>
+            </div>
+
+            <div class="form-group col-sm-2">
+              <label for="inputNum">Número</label>
+              <input type="number" class="form-control" id="inputNum" placeholder="Nº" required>
+            </div>
+
+            <div class="form-group col-sm-6">
+              <label for="inputEmail">E-mail</label>
+              <input type="text" class="form-control" id="inputEmail" placeholder="Digite seu E-mail" required>
+            </div>
+
+            <div class="form-group col-sm-6">
+              <label for="inputCof">Conferme E-mail</label>
+              <input type="text" class="form-control" id="inputEmail" placeholder="Comfirme seu E-mail" required>
+            </div>
+          </div>
+
+          <div class="form-row">
+            <div class="form-group col-sm-4">
+
+              <label for="inputTel">Telefone</label>
+              <input type="number" class="form-control" id="inputTel" placeholder="(xx) xxxx-xxxx" required>
+
+            </div>
+            <div class="form-group col-sm-4">
+
+              <label for="inputCel">Celular</label>
+              <input type="number" class="form-control" id="inputCel" placeholder="(xx) xxxxx-xxxx" required>
+
+            </div>
+
+          </div>
+
+          <div class="form-row">
+
+            <div class="form-group col-sm-3">
+              <label for="inputCidade">Cidade</label>
+              <input type="text" class="form-control" id="inputCidade" placeholder="Digite a Cidade" required>
+            </div>
+
+            <div class="for-group col-sm-3">
+              <label for="inputEst">Estado</label>
+              <select name="" id="inputEst" class="form-control">
+                <option value="" selected>Escolha</option>
+                <option value="SP">São paulo</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="MG">Minas Gereais</option>
+              </select>
+            </div>
+
+            <div class="for-group col-sm-3">
+
+              <label for="inputCEP">CEP</label>
+              <input type="text" class="form-control" id="inputCEP" placeholder="Digite seu CEP" required>
+
+            </div>
+
+          </div>
+
+          <div class="form-row">
+
+            <div class="for-group col-sm-12  mt-2">
+
+              <div class="form-check ">
+
+                <label for="" class="form-check-label">
+                  <input type="checkbox" class="form-check-input"> Desejo receber novidades por e-mail
+                </label>
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="form-row">
+            <div class="for-group col-sm-12  mt-2">
+              <button type="submit" class="btn btn-primary">Enviar</button>
+              <a tabindex="0" class="btn btn-secondary ml-2" role="button" data-toggle="popover" data-placement="right" trigger="focus" title="Precisa de ajuda" data-content=" O que podemos lhe ajudar">Ajuda</a>
+            </div>
+          </div>
+
+        </form>
+      </div>
+
     </div>
+    <!--Fim Formulário -->
+
+    <!-- inicio do Rodapé -->
+    <div class="row">
+      <div class="col-12 mb-3">
+        <hr>
+      </div>
+
+      <div class="col-sm-4">
+        <a class="navbar-brand " href="#"><img src="img/logo.png" alt="" width="70px"></a>
+        <h3>Faça a suas compras</h3>
+        <p>Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão.
+          Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um
+          impressor desconhecido pegou uma galera de tipos e os embaralhou para fazer um livro de
+          espécimes de tipos.
+        </p>
+      </div>
+
+      <div class="col-sm-4">
+        <h3>Menu</h3>
+        <div class="list-group text-center">
+          <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Perfil</a>
+          <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Serviço</a>
+          <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Depoimento</a>
+          <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Contatos</a>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+
+        <h3>Social</h3>
+        <div class="btn-group-vertical btn-block btn-group-lg" role="group">
+          <a href="#" class="btn btn-outline-primary"><i class="fa fa-facebook-official" aria-hidden="true"></i> Facebook</a>
+          <a href="#" class="btn btn-outline-info"><i class="fa fa-twitter-square" aria-hidden="true"></i> Twitter</a>
+          <a href="#" class="btn btn-outline-warning"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a>
+          <a href="#" class="btn btn-outline-danger"><i class="fa fa-youtube" aria-hidden="true"></i> YouTube</a>
+        </div>
+
+      </div>
+
+      <div class="col-12 mt-5">
+        <blockquote class="blockquote text-center">
+          <h4 class="mb-0">O sucesso não é garantido,
+            mas o fracasso é certo se voçê não estiver
+            emocionalmente envolvido em seu trabalho.
+          </h4>
+          <footer class="blockquote-footer">Biz Stone <cite title="Titulo">Fundado do Twitter</cite></footer>
+        </blockquote>
+      </div>
+    </div>
+
   </div>
-  <!--Fim Formulário -->
 
   <!--Icicio Modal -->
   <div class="modal fade" id="siteModal" tabindex="-1" role="dialg">
@@ -527,6 +690,15 @@
   <script src="node_modules/jquery/dist/jquery.js"></script>
   <script src="node_modules/popper.js/dist/umd/popper.js"></script>
   <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+
+  <script>
+    $(function() {
+      $('[data-toggle="popover"]').popover()
+
+    });
+  </script>
+
+
 </body>
 
 </html>
