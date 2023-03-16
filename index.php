f79fa170d331839a59fc0766b0bd1dcbbbada8a5
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-16 19:32:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
     <div class="container">
 
-      <a class="navbar-brand " href="#"><img src="img/logo.png" alt=""></a>
+      <a class="navbar-brand " href="#"><img src="img/logo.png" alt="" width="10px"></a>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
         <span class="navbar-toggler-icon"></span>
