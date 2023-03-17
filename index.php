7b5901ae62c284797751167ad19c12f77588b87a
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-16 21:10:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
     <div class="container">
 
-      <a class="navbar-brand " href="#"><img src="img/logo.png" alt="" width="70px"></a>
+      <a class="navbar-brand" href="#"><img src="img/logo.png" alt="" width="200px"></a>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
         <span class="navbar-toggler-icon"></span>
@@ -55,15 +55,14 @@
             <a href="#" class="nav-link dropdown-toggle text-dark" data-toggle="dropdown" id="navDrop">Social</a>
 
             <div class="dropdown-menu bg-white ">
-              <form>
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <form class="px-4 py-7">
+                <div class="form-group">
+                  <label for="exampleDropdownFormEmail1">Endereço de email</label>
+                  <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@exemplo.com">
                 </div>
-                <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                <div class="form-group">
+                  <label for="exampleDropdownFormPassword1">Senha</label>
+                  <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Senha">
                 </div>
                 <div class="mb-3 form-check">
                   <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -132,7 +131,7 @@
   <!-- Inicio do card -->
   <div class="container">
 
-    <div class="row">
+    <div class="row ">
 
       <div class="col-12 text-center my-5">
         <h1 class="display-3"><i class="fa fa-cogs text-primary" aria-hidden="true"></i> SEJA BEM VINDO</h1>
@@ -218,7 +217,7 @@
       </div>
     </div>
 
-    <div class="row justify-content-sm-center ">
+    <div class="row justify-content-sm-center">
 
       <div class="col-sm-6 col-md-4">
 
@@ -439,140 +438,7 @@
   </div>
   <!-- Fim do Jombontron -->
 
-  <!--Inicio Formulário -->
   <div class="container">
-
-    <div class="col-12 text-center my-5">
-      <h1 class="display-4"><i class="fa fa-paper-plane text-primary" aria-hidden="true"></i>Cadastre-se</h1>
-    </div>
-
-    <div class="row justify-content-center mb-5">
-
-      <div class="col-sm-12 col-md-10 col-lg-8">
-        <form>
-          <div class="form-row">
-            <div class="form-group col-sm-6">
-              <label for="inputNome">Nome</label>
-              <input type="text" class="form-control" id="inputNome" placeholder="Digite seu Nome" required>
-            </div>
-
-            <div class="form-group col-sm-6">
-              <label for="inputSobrenome">Sobrenome</label>
-              <input type="text" class="form-control" id="inputNome" placeholder="Digite seu Sobrenome" required>
-            </div>
-          </div>
-
-          <div class="form-row">
-            <div class="form-group col-sm-3">
-              <label for="inputRg">RG</label>
-              <input type="number" class="form-control" id="inputRg" placeholder="Digite seu RG" maxlength="13" required>
-
-            </div>
-            <div class="form-group col-sm-3">
-              <label for="inputCPF">CPF</label>
-              <input type="number" class="form-control" id="inputCPF" placeholder="Digite seu CPF" maxlength="13" required>
-
-            </div>
-            <div class="form-group col-sm-3">
-              <label for="inputDate">Data de Nacimento</label>
-              <input type="date" class="form-control" id="inputDate" placeholder="" required>
-            </div>
-
-          </div>
-
-          <div class="form-row">
-
-            <div class="form-group col-sm-6">
-              <label for="inputEnd">Endereço</label>
-              <input type="text" class="form-control" id="inputEnd" placeholder="Digite seu Enderço" required>
-            </div>
-
-            <div class="form-group col-sm-2">
-              <label for="inputNum">Número</label>
-              <input type="number" class="form-control" id="inputNum" placeholder="Nº" required>
-            </div>
-
-            <div class="form-group col-sm-6">
-              <label for="inputEmail">E-mail</label>
-              <input type="text" class="form-control" id="inputEmail" placeholder="Digite seu E-mail" required>
-            </div>
-
-            <div class="form-group col-sm-6">
-              <label for="inputCof">Conferme E-mail</label>
-              <input type="text" class="form-control" id="inputEmail" placeholder="Comfirme seu E-mail" required>
-            </div>
-          </div>
-
-          <div class="form-row">
-            <div class="form-group col-sm-4">
-
-              <label for="inputTel">Telefone</label>
-              <input type="number" class="form-control" id="inputTel" placeholder="(xx) xxxx-xxxx" required>
-
-            </div>
-            <div class="form-group col-sm-4">
-
-              <label for="inputCel">Celular</label>
-              <input type="number" class="form-control" id="inputCel" placeholder="(xx) xxxxx-xxxx" required>
-
-            </div>
-
-          </div>
-
-          <div class="form-row">
-
-            <div class="form-group col-sm-3">
-              <label for="inputCidade">Cidade</label>
-              <input type="text" class="form-control" id="inputCidade" placeholder="Digite a Cidade" required>
-            </div>
-
-            <div class="for-group col-sm-3">
-              <label for="inputEst">Estado</label>
-              <select name="" id="inputEst" class="form-control">
-                <option value="" selected>Escolha</option>
-                <option value="SP">São paulo</option>
-                <option value="RJ">Rio de Janeiro</option>
-                <option value="MG">Minas Gereais</option>
-              </select>
-            </div>
-
-            <div class="for-group col-sm-3">
-
-              <label for="inputCEP">CEP</label>
-              <input type="text" class="form-control" id="inputCEP" placeholder="Digite seu CEP" required>
-
-            </div>
-
-          </div>
-
-          <div class="form-row">
-
-            <div class="for-group col-sm-12  mt-2">
-
-              <div class="form-check ">
-
-                <label for="" class="form-check-label">
-                  <input type="checkbox" class="form-check-input"> Desejo receber novidades por e-mail
-                </label>
-
-              </div>
-
-            </div>
-
-          </div>
-
-          <div class="form-row">
-            <div class="for-group col-sm-12  mt-2">
-              <button type="submit" class="btn btn-primary">Enviar</button>
-              <a tabindex="0" class="btn btn-secondary ml-2" role="button" data-toggle="popover" data-placement="right" trigger="focus" title="Precisa de ajuda" data-content=" O que podemos lhe ajudar">Ajuda</a>
-            </div>
-          </div>
-
-        </form>
-      </div>
-
-    </div>
-    <!--Fim Formulário -->
 
     <!-- inicio do Rodapé -->
     <div class="row">
@@ -612,7 +478,7 @@
 
       </div>
 
-      <div class="col-12 mt-5">
+      <div class="col-12 mt-">
         <blockquote class="blockquote text-center">
           <h4 class="mb-0">O sucesso não é garantido,
             mas o fracasso é certo se voçê não estiver
@@ -686,12 +552,6 @@
   <script src="node_modules/popper.js/dist/umd/popper.js"></script>
   <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
 
-  <script>
-    $(function() {
-      $('[data-toggle="popover"]').popover()
-
-    });
-  </script>
 
 
 </body>
